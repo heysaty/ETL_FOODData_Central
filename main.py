6c044345b3bfd1e6361b3ea7f7a433e6e81aac8f
heysaty <satyam@gkmit.co>
2022-11-24 23:28:05 +0530

    nutreients preprocessed from response

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from transformation.preprocessing_nutrients import get_nutrients
+import pandas as pd
+import time
 
 
-get_nutrients('roasted peanuts')
+start = time.time()
+df = pd.read_csv('foods.csv')
+print(df)
+for food in df['food']:
+# food= input("food: ")
+    x=time.time()
+    print(get_nutrients(food))
+    print('---------------------------------------------------\n')
+    t=time.time()
+    print('------------',t-x,'-------------')
+print(time.time() - start)
--- transformation/preprocessing_nutrients.py
+++ transformation/preprocessing_nutrients.py
@@ -6,7 +6,6 @@ from extraction import search_food
 from transformation.nutrients import get_nutrients_name
 
 
-
 def get_nutrients(food_item):
     api_response = search_food.search_food(food_item)
 
@@ -15,16 +14,19 @@ def get_nutrients(food_item):
             # pprint(api_response['foods'][1]['foodNutrients'])
 
             nutrients_json = api_response['foods'][1]['foodNutrients']
+            nutrients_dict={}
             for item in nutrients_json:
                 if item['nutrientName'] in get_nutrients_name():
                     # pprint(item)
-                    print(item['nutrientName'], ' = ', str(item['value']), ' ', item['unitName'])
+                    # print([item['nutrientName'], str
+                    # (item['value']) + ' ' + item['unitName']])
+                    nutrients_dict[item['nutrientName']] = str(item['value']) + ' ' + item['unitName']
                     # print('\n\n----------------------------------------------------------------------')
-
+            return nutrients_dict
 
         else:
             print('NO Such Food Items !!!')
+            return None
     except:
         print('API Connection Error !!!')
-
-
+        return None
