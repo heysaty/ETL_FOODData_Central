00df439c86adc3a78380d21c91770cded4019f27
heysaty <satyam@gkmit.co>
2022-11-27 16:11:58 +0530

    new sb schema created

--- loading/create_table.py
+++ loading/create_table.py
@@ -1,29 +1,97 @@
+
 def create_fooditems():
     query = """CREATE TABLE fooditems(
             id SERIAL PRIMARY KEY ,
             food_item VARCHAR(100),
             description VARCHAR(300),
-            nutrients_id INT REFERENCES nutrients(id)
+            nutrients_log_id INT REFERENCES nutrients_log(id)
         );"""
 
     return query
 
 
-def create_nutrients():
-    query = """CREATE TABLE nutrients(
-        id SERIAL PRIMARY KEY ,
-        protein VARCHAR(50),
-        fiber VARCHAR(50),
-        iron VARCHAR(50),
-        cholesterol VARCHAR(50),
-        lipid VARCHAR(50),
-        carbohydrate VARCHAR(50),
-        energy VARCHAR(50),
-        water VARCHAR(50),
-        calcium VARCHAR(50),
-        caffeine VARCHAR(50),
-        sugar VARCHAR(50)
-        
-    );"""
-
+def create_nutrientslog():
+    query = """CREATE TABLE nutrients_log(
+               id SERIAL PRIMARY KEY ,
+               macro_nutrients_id INT REFERENCES macro_nutrients(id),
+               micro_nutrients_id INT REFERENCES micro_nutrients(id)
+          
+               
+           );"""
     return query
+
+def create_macronutrients():
+    query = """CREATE TABLE macro_nutrients(
+               id SERIAL PRIMARY KEY ,
+               Protein VARCHAR(100),
+               Carbohydrate VARCHAR(100),
+               Energy VARCHAR(100),
+               fats_id INT REFERENCES fats(id)
+           );"""
+    return query
+
+
+def create_micronutrients():
+    query = """CREATE TABLE micro_nutrients(
+               id SERIAL PRIMARY KEY ,
+               minerals_id INT REFERENCES minerals(id),
+               vitamins_id INT REFERENCES vitamins(id)
+           );"""
+    return query
+
+
+def create_fats():
+    query = """CREATE TABLE fats(
+               id SERIAL PRIMARY KEY ,
+               lipid VARCHAR(100),
+               Sugars VARCHAR(100),
+               Cholesterol VARCHAR(100),
+               Fatty_acids VARCHAR(100)
+           );"""
+    return query
+
+
+def create_minerals():
+    query = """CREATE TABLE minerals(
+               id SERIAL PRIMARY KEY ,
+               Calcium VARCHAR(100),
+               Iron VARCHAR(100),
+               Magnesium VARCHAR(100),
+               Sodium VARCHAR(100)
+                              
+            
+           );"""
+    return query
+
+
+def create_vitamins():
+    query = """CREATE TABLE vitamins(
+               id SERIAL PRIMARY KEY ,
+               Vitamin_A VARCHAR(100),
+               Vitamin_E VARCHAR(100),
+               Vitamin_D VARCHAR(100),
+               Vitamin_C VARCHAR(100),
+               Vitamin_B12 VARCHAR(100)
+               
+           );"""
+    return query
+
+
+# def create_nutrients():
+#     query = """CREATE TABLE nutrients(
+#         id SERIAL PRIMARY KEY ,
+#         protein VARCHAR(50),
+#         fiber VARCHAR(50),
+#         iron VARCHAR(50),
+#         cholesterol VARCHAR(50),
+#         lipid VARCHAR(50),
+#         carbohydrate VARCHAR(50),
+#         energy VARCHAR(50),
+#         water VARCHAR(50),
+#         calcium VARCHAR(50),
+#         caffeine VARCHAR(50),
+#         sugar VARCHAR(50)
+#
+#     );"""
+#
+#     return query
--- loading/query_execution.py
+++ loading/query_execution.py
@@ -5,23 +5,28 @@ from loading import create_table
 from transformation import preprocessing_nutrients
 from loading.insert_data import insert_fooditem_query, insert_nutrients_query
 
-queries = [create_table.create_nutrients(), create_table.create_fooditems(), 'select * from fooditems;']
+queries = [create_table.create_minerals(),create_table.create_vitamins(),create_table.create_fats(),
+           create_table.create_macronutrients(),create_table.create_micronutrients(),
+           create_table.create_nutrientslog(),create_table.create_fooditems(), 'select * from fooditems;']
 
 
 # nutrients_dict, food_description = preprocessing_nutrients.get_nutrients()
 
 def create_table_execution():
     for query in queries:
-        try:
-            cur = conn.cursor()
-            print(query)
-            print(cur.execute(query))
-            conn.commit()
-            cur.close()
+        print(query)
+        # #     try:
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        cur.close()
             # conn.close()
-        except:
-            continue
+        # except:
+        #     continue
 
+
+create_table_execution()
+conn.close()
 
 
 def insert_data(fooditem):
